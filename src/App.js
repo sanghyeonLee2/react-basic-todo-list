ece70dc24b42d21c152baf31664fae2f1e56a747
sanghyeonLee <95309008+sanghyeonLee2@users.noreply.github.com>
2023-06-27 17:48:36 +0900

    feat: todo-list 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useRef } from "react"; //Dom에 직접 접근하기 위해 사용,  리렌더링X. 컴포넌트의 속성만 조회&수정
+import "./App.css";
 
 function App() {
+  const [createTodo, setCreateTodo] = useState([]);
+  const [updateMode, setUpdateMode] = useState();
+  const inputRef = useRef(null);
+
+  const deleteTodo = (idx) => {
+    console.log(idx);
+    console.log(createTodo);
+    createTodo.splice(idx, 1);
+    setCreateTodo([...createTodo]);
+  };
+
+  const submitTodo = (e) => {
+    e.preventDefault();
+    const newTodo = e.target.firstChild.value;
+    setCreateTodo([...createTodo, newTodo]);
+    resetHandler();
+  };
+
+  const resetHandler = () => {
+    inputRef.current.value = "";
+  };
+
+  const updateTodo = (idx) => {
+    setUpdateMode(idx);
+  };
+
+  const updateComplete = (e, idx) => {
+    e.preventDefault();
+    const updateTodoValue = e.target.firstChild.value;
+    createTodo[idx] = updateTodoValue;
+    console.log(createTodo);
+    setUpdateMode(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo-List</h1>
+      <form onSubmit={submitTodo}>
+        <input
+          type="text"
+          placeholder="할일을 입력하세요"
+          ref={inputRef}
+          required
+        />
+        <button type="submit">추가</button>
+      </form>
+      <>
+        <ul>
+          {createTodo.map((todo, idx) => {
+            return (
+              <li key={idx}>
+                {idx === updateMode ? (
+                  <>
+                    <form onSubmit={(e) => updateComplete(e, idx)}>
+                      <input
+                        type="text"
+                        placeholder="할일을 입력하세요"
+                        defaultValue={todo}
+                        required
+                      />
+                      <button type="submit">수정완료</button>
+                    </form>
+                  </>
+                ) : (
+                  <>
+                    {todo}
+                    <button type="button" onClick={() => updateTodo(idx)}>
+                      수정
+                    </button>
+                  </>
+                )}
+                <>
+                  <button type="button" onClick={() => deleteTodo(idx)}>
+                    삭제
+                  </button>
+                </>
+              </li>
+            );
+          })}
+        </ul>
+      </>
     </div>
   );
 }
